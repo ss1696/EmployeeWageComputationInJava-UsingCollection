9cb97a78ee4441a6c8893f36627b638a44d7d6d9
shashank srivastava <shashank.1696@gmail.com>
2020-07-20 18:26:59 +0530

    Added: To Store EmployeeWage for Particular Company

---
+++ CompanyEmployeeWage.java
@@ -0,0 +1,27 @@
+public class CompanyEmployeeWage
+{
+
+	public final int wage_per_hour;
+	public final int total_working_days;
+ 	public final int total_working_hour;
+	public final String company_name;
+	public	int totalWage = 0;
+
+	public CompanyEmpWage(String company_name, int wage_per_hour, int total_working_days, int total_working_hour)
+	{
+		this.company_name = company_name;
+		this.wage_per_hour = wage_per_hour;
+		this.total_working_days = total_working_days;
+		this.total_working_hour = total_working_hour;
+	}
+
+	public void setTotalWage(int totalWage)
+	{
+		this.totalWage = totalWage;
+	}
+
+	@Override
+	public String toString(){
+		return "total emp wage for company "+company_name+" is "+totalWage;
+	}
+}
