3f5c4182cc2900eae379a32e6b6a673ba3fc2756
shashank srivastava <shashank.1696@gmail.com>
2020-07-20 19:16:23 +0530

    Refactor: To Manage Employee Wage of Multiple Companies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,79 +1,84 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 interface ICompanyEmplopyeeWage
 {
-        void addCompanyEmpWage(String company_name, int wage_per_hour, int total_working_days, int total_working_hour);
+        void addCompanyEmployeeWage(String company_name, int wage_per_hour, int total_working_days, int total_working_hour);
         void computeWage();
-        int computeWage(CompanyEmployeeWage CompanyEmpWage);
+        int computeWage(CompanyEmployeeWage CompanyEmployeeWage);
 }
 
-public class EmployeeWage {
-		public static byte FULL_DAY_HOUR = 8;
-		public static byte HALF_DAY_HOUR = 4;
-		
-		public int totalWage = 0;
-		public int numOfCompany = 0;
-		
-		public CompanyEmployeeWage[] comEmpArray;
-		
-		public EmployeeWage()
+class TestEmployee implements ICompanyEmplopyeeWage
+{
+	public static byte FULL_DAY_HOUR = 8;
+    public static byte HALF_DAY_HOUR = 4;
+
+	public int totalWage = 0;
+
+	CompanyEmployeeWage comEmp = new CompanyEmployeeWage(company_name, wage_per_hour, total_working_days, total_working_hour);
+
+	public ArrayList<CompanyEmployeeWage> comEmpArray = new ArrayList<CompanyEmployeeWage>();
+
+	public void addCompanyEmployeeWage(String company_name, int wage_per_hour, int total_working_days, int total_working_hour)
+	{
+		comEmpArray.add(CompanyEmployeeWage(comEmp));
+	}
+
+	
+
+	public void computeWage()
+        {
+		Iterator<CompanyEmployeeWage> it = comEmpArray.iterator();
+		while(it.hasNext())
 		{
-			comEmpArray = new CompanyEmployeeWage[5];
+			CompanyEmployeeWage ce = it.next();
 		}
-		
-		public void addCompanyEmpWage(String company_name, int wage_per_hour, int total_working_days, int total_working_hour)
+	}
+
+	public int computeWage(CompanyEmployeeWage CompanyEmpWage)
+	{
+		int empWageFullDay = 0;
+		int empWageHalfDay = 0;
+		int totalWorkingDays = 0;
+		int workingHour = 0;
+		int wage = 0;
+
+		while(workingHour <= CompanyEmpWage.total_working_hour && totalWorkingDays <= CompanyEmpWage.total_working_days)
 		{
-			comEmpArray[numOfCompany] = new CompanyEmployeeWage(company_name, wage_per_hour, total_working_days, total_working_hour);
-			numOfCompany++;
-		}
-		
-		public void computeWage()
-		    {
-			for(int i=0; i<numOfCompany; i++)
+			double working_time = Math.floor(Math.random() * 10) % 3;
+			int day = (int) working_time;
+			switch(day)
 			{
-				comEmpArray[i].setTotalWage(this.computeWage(comEmpArray[i]));
-				System.out.println(comEmpArray[i]);
+			case 0:
+				empWageHalfDay = CompanyEmpWage.wage_per_hour * HALF_DAY_HOUR;
+				wage = wage + empWageHalfDay;
+        	        	workingHour = workingHour + HALF_DAY_HOUR;
+				break;
+        		case 1:
+				empWageFullDay = CompanyEmpWage.wage_per_hour * FULL_DAY_HOUR;
+				wage = wage + empWageFullDay;
+        	        	workingHour = workingHour + FULL_DAY_HOUR;
+				break;
+			default:
+				System.out.println("employee is absent");
+				break;
 			}
+			totalWorkingDays++;
+			totalWage = totalWage + wage;
 		}
-		
-		public int computeWage(CompanyEmployeeWage CompanyEmpWage)
-		{
-			int empWageFullDay = 0;
-			int empWageHalfDay = 0;
-			int totalWorkingDays = 0;
-			int workingHour = 0;
-			int wage = 0;
-		
-			while(workingHour <= CompanyEmpWage.total_working_hour && totalWorkingDays <= CompanyEmpWage.total_working_days)
-			{
-				double working_time = Math.floor(Math.random() * 10) % 3;
-				int day = (int) working_time;
-				switch(day)
-				{
-				case 0:
-					empWageHalfDay = CompanyEmpWage.wage_per_hour * HALF_DAY_HOUR;
-					wage = wage + empWageHalfDay;
-		    	        	workingHour = workingHour + HALF_DAY_HOUR;
-					break;
-		    		case 1:
-					empWageFullDay = CompanyEmpWage.wage_per_hour * FULL_DAY_HOUR;
-					wage = wage + empWageFullDay;
-		    	        	workingHour = workingHour + FULL_DAY_HOUR;
-					break;
-				default:
-					System.out.println("employee is absent");
-					break;
-				}
-				totalWorkingDays++;
-				totalWage = totalWage + wage;
-			}
 		System.out.println("monthly wage = "+totalWage);
 		return totalWage;
-		}
-		
-		public static void main(String[] args)
-		{
-			EmployeeWage empWage = new EmployeeWage();
-			empWage.addCompanyEmpWage("DMart",20,2,10);
-			empWage.addCompanyEmpWage("tcs",25,20,20);
-			empWage.computeWage();
-		}
+	}
+}
+
+public class EmployeeWage
+{
+	public static void main(String[] args)
+	{
+		TestEmployee t = new TestEmployee();
+
+		t.addCompanyEmployeeWage("DMart",20,2,10);
+		t.addCompanyEmployeeWage("tcs",25,20,20);
+		t.computeWage();
+	}
 }
