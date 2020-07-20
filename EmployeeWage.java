06b8d22df203be3ed64dfdb04886c93cef27a117
shashank srivastava <shashank.1696@gmail.com>
2020-07-20 17:21:52 +0530

    Added: Employee Wage For totalWorkHrs and Days

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,50 +1,52 @@
 public class EmployeeWage {
 
-	public static void main(String[] agrs)
+	public static final int WAGE_PER_HOUR = 20;
+    public static final byte FULL_DAY_HOUR = 8;
+    public static final byte HALF_DAY_HOUR = 4;
+    public static final int TOTAL_WORKING_DAYS = 20;
+    public static final int TOTAL_WORKING_HOURS = 100;
+
+public static void main(String[] agrs)
+{
+	System.out.println("calculate employee wage");
+	//constants
+	int empWage = 0;
+	int empWageFullDay = 0;
+            int empWageHalfDay = 0;
+            int totalWorkingDays = 0;
+            int totalWorkingHour = 0;
+            int totalWage = 0;
+            int wage = 0;
+	int is_present = 1;
+	int workingHour = 0;
+	//Computation
+	while(totalWorkingDays <= TOTAL_WORKING_DAYS && totalWorkingHour <= TOTAL_WORKING_HOURS)
 	{
-		System.out.println("calculate employee wage");
-		//constants
-		int totalWorkingHour = 100;
-		int workingHour = 0;
-		int is_present = 1;
-		int empWage = 0;
-		int wagePerHour = 20;
-                byte fullDayHour = 8;
-                byte halfDayHour = 4;
-                int empWageFullfDay = 0;
-		int empWageHalfDay = 0;
-		int workingDays = 20;
-		int totalWage = 0;
-		int wage = 0;
-		//Computation
-		while(workingDays >= 0 && totalWorkingHour >= 0)
-		{
-			double workingTime = Math.floor(Math.random() * 10) % 3;
-               		int day = (int) workingTime;
-			switch(day)
-               		{
-                		case 0:
-                        		empWageHalfDay = wagePerHour * halfDayHour;
-					wage = wage + empWageHalfDay;
-                        		System.out.println("employee half day");
-                        		workingHour = halfDayHour;
-					break;
-               			case 1:
-                        		empWageFullfDay = wagePerHour * fullDayHour;
-                        		wage = wage + empWageFullfDay;
-					workingHour = fullDayHour;
-					System.out.println("employee full day");
-                        		break;
-                		default:
-                        		System.out.println("employee wage absent");
-                        		break;
+		int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+			switch(empCheck)
+	            		{
+	            		case 0:
+	                    		empWageHalfDay = WAGE_PER_HOUR * HALF_DAY_HOUR;
+	                    		wage = wage + empWageHalfDay;
+	                    		System.out.println("employee half day Wage ="+wage);
+	                    		workingHour = HALF_DAY_HOUR;
+	                    		break;
+	           			case 1:
+	                    		empWageFullDay = WAGE_PER_HOUR * FULL_DAY_HOUR;
+	                    		wage = wage + empWageFullDay;
+	                    		System.out.println("employee full day Wage ="+wage);
+	                    		workingHour = FULL_DAY_HOUR;
+	                    		break;
+	            		default:
+	                    		System.out.println("employee wage = 0");
+	                    		break;
 			}
-			totalWorkingHour = totalWorkingHour - workingHour;
-			workingDays--;
-			totalWage = totalWage + wage;
-		}
-		System.out.println("monthly wage = "+totalWage);
+		totalWorkingHour = totalWorkingHour + workingHour;
+		totalWorkingDays++;
+		totalWage = totalWage + wage;
 	}
+	System.out.println("monthly wage = "+totalWage);
+}
 
 
 }
