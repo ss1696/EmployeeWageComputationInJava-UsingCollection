1e1bf4fd5b5f37cbf607173f42264a8db6dbd6ee
shashank srivastava <shashank.1696@gmail.com>
2020-07-20 16:56:55 +0530

    Added: Employee Daily Wages

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,21 +4,29 @@ public class EmployeeWage {
 	{
 		System.out.println("calculate employee wage");
 		//constants
-		int IS_FULL_TIME = 1;
+		int wagePerHour = 20;
+                int fullDayHour = 1;
+                int empWage = 0;
+		int empWageHalfDay = 0;
+		int is_present = 1;
 		//Computation
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME)
-			System.out.println("present");
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == 1)
+		{
+			empWage = wagePerHour * 8;
+                        System.out.println("employee wage = "+empWage);
+			System.out.println("present full time");
+		}
+		else if (empCheck == 2)
+		{
+			empWageHalfDay = wagePerHour * 4;
+                	System.out.println("employee wage = "+empWageHalfDay);
+		}
 		else
-			System.out.println("absent");
-
-		int WagePerHour = 20;
-		int FullDayHour = 8;
-		int EmpWage = 0;
-		EmpWage = WagePerHour * FullDayHour;
-		System.out.println("employee wage = "+EmpWage);
+		{
+			System.out.println("employee is absent ");
+		}
 	}
-
 
 
 }
