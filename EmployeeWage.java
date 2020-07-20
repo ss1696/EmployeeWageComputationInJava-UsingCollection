b3d8551069fec5e0d00c7de5327e9b90fbc1bad3
shashank srivastava <shashank.1696@gmail.com>
2020-07-20 17:04:44 +0530

    Added: Switch Case Solving

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,7 +2,7 @@ public class EmployeeWage {
 
 	public static void main(String[] agrs)
 	{
-		System.out.println("calculate employee wage");
+			System.out.println("calculate employee wage");
 		//constants
 		int is_present = 1;
 		int empWage = 0;
@@ -10,25 +10,35 @@ public class EmployeeWage {
                 byte fullDayHour = 8;
                 byte halfDayHour = 4;
                 int empWageFullfDay = 0;
-                int empWageHalfDay = 0;
-
+		int empWageHalfDay = 0;
+		int workingDays = 20;
+		int totalWage = 0;
+		int wage = 0;
 		//Computation
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		int empCheckFullPartAbsent = (int) empCheck;
-		switch(empCheckFullPartAbsent)
-               	{
-	               	case 0:
-        	               	empWageHalfDay = wagePerHour * halfDayHour;
-                        	System.out.println("employee wage = "+empWageHalfDay);
-                        	break;
-               		case 1:
-                        	empWageFullfDay = wagePerHour * fullDayHour;
-                        	System.out.println("employee wage = "+empWageFullfDay);
-                        	break;
-                	default:
-                        	System.out.println("employee is absent");
-                        	break;
+		while(workingDays >= 0)
+		{
+			double empCheck = Math.floor(Math.random() * 10) % 3;
+               		int empCheckFullPartAbsent = (int) empCheck;
+			switch(empCheckFullPartAbsent)
+            		{
+	               		case 0:
+                        		empWageHalfDay = wagePerHour * halfDayHour;
+					wage = wage + empWageHalfDay;
+                        		System.out.println("employee half day");
+                        		break;
+               			case 1:
+                        		empWageFullfDay = wagePerHour * fullDayHour;
+                        		wage = wage + empWageFullfDay;
+					System.out.println("employee full day");
+                        		break;
+                		default:
+                        		System.out.println("employee absent");
+                        		break;
+			}
+			workingDays--;
+			totalWage = totalWage + wage;
 		}
+		System.out.println("monthly wage = "+totalWage);
 	}
 
 
